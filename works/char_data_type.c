46e15406274af8e53017f7d60b71fccc7e4e0680
user <shoaibkhana2000@gmail.com>
2019-10-28 14:08:58 +0200

    20191028-14_08

--- works/char_data_type.c
+++ works/char_data_type.c
@@ -1,27 +1,28 @@
-#include<stdio.h> //we will use printf and scanf
+#include<stdio.h> // we will use printf and scanf
 
-void main() // main function is "ēntry point" and it should obliga$
+void main() // main function is "entry point" and it should obligatory
  {
  char a='A';
- printf("a variable value as symbol: %c\n",a);
- printf("a variable value as decimal number: %d\n",a);
- printf("a variable value as octal number: %o\n",a);
- printf("a variable value as hexadecimal number: %x\n",a);
- 
-printf ("Dear user,please enter any symbol:");
-scanf("%c",&a);
-printf("your selected value as symbol: %c\n",a);
-printf("your selected value as decimal number: %d\n",a);
-printf("your selected value as octal number: %o\n",a);
-printf("your selected value as hexadecimal number: %x\n",a);
+ printf("a variable value as symbol: %c\n", a);
+ printf("a variable value as decimal number: %d\n", a);
+ printf("a variable value as octal number: %o\n", a);
+ printf("a variable value as hexadecimal number: %x\n", a);
 
-char b=127;
-printf("b char variable value as decimal number (?127): %d\n",b);
+ printf("Dear user, please enter any one symbol: ");
+ scanf("%c",&a);
+ printf("Your selected value as symbol: %c\n", a);
+ printf("Your selected value as decimal number: %d\n", a);
+ printf("Your selected value as octal number: %o\n", a);
+ printf("Your selected value as hexadecimal number: %x\n", a);
 
-char c=128;
-printf("c char variable value as decimal number (?128): %d\n",c);
+ char b=127;
+ printf("b char variable value as decimal namber (?127): %d\n",b);
 
-char d=129;
-printf("d char variable value as decimal number (?129):%d\n",d);
+ char c=128;
+ printf("c char variable value as decimal namber (?128): %d\n",c);
+
+ char d=129;
+ printf("d char variable value as decimal namber (?129): %d\n",d);
  }
+// we use void main, because our main doesn't return
 
---
+++ works/printf_tests.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+ {
+ float a=1.23456  b=7.89012, c=34.5678;
+ printf("floats: %4.2f %+.0e %E \n",A=1.23456);
+ printf("floats: %4.2f %+.0e %E \n",B=7.89012);
+
+ // write code to get following result  (use printf):
+/*
+cmd:>gcc printf_tests.c
+cmd:>./a.out
+1.2          7.89012 34.5678
+1.23         7.8901  34.56780
+1.23456 7.890 34.567800
+cmd:>
+*/
+ // Rule - don't use (put) spaces " "!!!
+ }
+ 
+
+
+
